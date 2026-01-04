f9f0781b4a423ddb4e98c7b20390c5c8204f8152
Tiago Miranda Hoelzle <tiagohoelzle@gmail.com>
2026-01-04 17:35:17 -0300

    Add files via upload

---
+++ Weather-App/main.py
@@ -0,0 +1,36 @@
+# Importando a biblioteca request
+import requests
+
+#Informações da API
+api_key = "f33e35b2762492a05cabd2f89a04851f" 
+units = "metric"
+lang = "pt_br"
+
+def getCityName(city):
+
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={units}&lang={lang}")
+
+    # 200 é o status de sucesso na comunicação HTTP
+    if response.status_code == 200:
+        data = response.json()
+
+        # Retorna os dados da API em formato JSON
+        return data
+    else:
+        print(f"Falha na requisição: {response.status_code}")    
+
+# Recebe o nome da cidade a ser consultada(
+cityName = str(input("Digite o nome de uma cidade: ")).strip().capitalize()
+
+# Armazena os dados retornados da função em uma variavel
+weatherInfo = getCityName(cityName)
+
+# Verifica se a variável contém dados
+if weatherInfo:
+
+    # Acessa as chaves específicas do JSON retornado pela API
+    print(f"\nCidade: {weatherInfo['name']}, {weatherInfo['sys']['country']}")
+    print(f"Temperatura: {weatherInfo['main']['temp']} Cº")
+    print(f"Humidade: {weatherInfo['main']['humidity']} %")
+    print(f"Sensação Térmica: {weatherInfo['main']['feels_like']} Cº")
+    print(f"Descrição: {weatherInfo['weather'][0]['description']}")
