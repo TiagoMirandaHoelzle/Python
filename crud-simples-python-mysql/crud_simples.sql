196051ccdd652762ef52ef23db441b4fa81b81a8
Tiago Miranda Hoelzle <tiagohoelzle@gmail.com>
2026-01-05 14:00:27 -0300

    Add files via upload

---
+++ crud-simples-python-mysql/crud_simples.sql
@@ -0,0 +1,18 @@
+DROP SCHEMA IF EXISTS crud_simples;
+CREATE SCHEMA crud_simples;
+USE crud_simples;
+
+CREATE TABLE Produtos(
+	id_produto INT PRIMARY KEY AUTO_INCREMENT,
+    nome_produto VARCHAR(255) NOT NULL,
+    descricao_produto VARCHAR(500) NOT NULL,
+    preco_produto DECIMAL(10,2) NOT NULL,
+    desconto_produto INT DEFAULT (0),
+    valor_desconto DECIMAL(10,2) GENERATED ALWAYS AS ((preco_produto * desconto_produto)/100) STORED,
+    preco_final_produto DECIMAL(10,2) GENERATED ALWAYS AS (preco_produto - valor_desconto) STORED,
+    estoque_produto INT NOT NULL DEFAULT (1)
+);
+
+SELECT * FROM Produtos;
+
+UPDATE Produtos SET nome_produto = "Camisa Nike", preco_produto = 190.00, desconto_produto = 0  WHERE id_produto = 1;
